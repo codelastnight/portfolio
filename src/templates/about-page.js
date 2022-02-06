ba5eaf59f1e6db9300383051692b029c36a5df99
last night <fujizhang2525@gmail.com>
2022-02-06 01:08:47 -0500

    ios fix and about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,24 +3,19 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import * as c from './about-page.module.scss';
 
 // eslint-disable-next-line
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+    <section className="grid">
+      <div className="col2 end1__r ">
+          <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
+          <PageContent className="content" content={content} />
       </div>
     </section>
   );
