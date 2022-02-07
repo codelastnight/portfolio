bc0ec020194584f0ebacb949c45021a4cc889559
last night <fujizhang2525@gmail.com>
2022-02-07 01:44:43 -0500

    mainheading animation

--- src/components/MainHeading/MainHeading.js
+++ src/components/MainHeading/MainHeading.js
@@ -1,9 +1,10 @@
 import * as c from './mainheading.module.scss';
 import React from "react";
+import {useEffect} from 'react';
 import Button from '../Button';
 import LogoIcon from '../LogoIcon';
-import {motion} from 'framer-motion';
-
+import { motion ,useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer';
 
 const container = {
     hidden: { opacity: 0},
@@ -26,8 +27,8 @@ const variants = {
         }
   }
 const links = {
-    hidden2: { opacity: 0},
-    show2: { 
+    hidden: { opacity: 0},
+    show: { 
         opacity: 1,
         transition: {
             staggerChildren: 0.2,
@@ -37,10 +38,21 @@ const links = {
           }
         }
   }
-
+const line = {
+    hidden: {
+         pathLength: 0 
+    },
+    show: {
+        pathLength: 1,
+        transition: {
+            duration: 1,
+            delay: 0.5
+        }
+    }
+}
 const link = {
-    hidden2: { opacity: 0 },
-    show2: { 
+    hidden: { opacity: 0 },
+    show: { 
         opacity: 1,
         transition: {
             duration: 0.1
@@ -50,19 +62,38 @@ const link = {
 
 
 const MainHeading = ({}) => {
-    
+    const [ ref, inView] = useInView();
+    const controls = useAnimation();
+    const linkAni = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            controls.stop();
+            linkAni.stop();
+            controls.start("show");
+            linkAni.start("show");
+
+        }
+        if (!inView) {
+            controls.stop();
+            linkAni.stop();
+            controls.start("hidden");
+            linkAni.start("hidden");
+
+        }
+    }, [inView]);
+
     return (
         <React.Fragment>
              <motion.section className={`z5 ${c.mainheading__wrapper}`} 
                 initial="hidden"
-                animate="show"  
+                animate={controls}  
                 variants={container}
              > 
              
             <div className={`grid ${c.mainheading}`}>
-                <motion.h4 initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }} 
-                            transition={{duration: 0}} className={`row1 col3 span2 text`} >hello, my name is simon</motion.h4>
+                <motion.h4 variants={variants}
+                            className={`row1 col3 span2 text`} >hello, my name is simon</motion.h4>
 
                 <div className={`row2 col2 end1__r flex flexgap align__cente  ${c.titletext} `}>
                     <motion.h1 variants={variants} className={`${c.maxcontent} ${c.animate}`}>I'm a</motion.h1>
@@ -72,9 +103,9 @@ const MainHeading = ({}) => {
                         </motion.div> 
                         <svg className={`width100 ${c.svg}  ${c.margin1rem}`} viewBox="0 0 271 105" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <motion.path 
-                            initial={{ pathLength: 0 }}
-                            animate={{ pathLength: 1 }} 
-                            transition={{duration: 1, delay: 0.5}}
+                            initial="hidden"
+                            animate={linkAni}
+                            variants={line}
                             d="M2 82.0703C37.4877 57.3352 72.0715 26.0492 111.767 9.96763C124.767 4.70099 154.129 -4.08814 166.32 8.32062C172.012 14.1139 170.302 30.0883 168.957 37.2349C164.765 59.5141 138.199 74.3119 121.326 82.9853C113.067 87.2309 104.74 90.9453 97.0981 96.5274C95.8164 97.4637 89.4838 104.391 87.2093 102.749C85.8746 101.786 86.4714 96.0861 86.55 94.5144C87.0137 85.2457 95.8244 81.5675 102.537 78.4102C125.636 67.5454 150.573 60.9331 174.231 51.6005C183.973 47.7579 211.098 35.2735 201.261 59.1036C199.476 63.4291 176.383 80.4233 188.158 80.4233C194.684 80.4233 198.219 76.3477 203.898 73.1947C213.97 67.6028 225.82 62.8604 236.367 58.3716C247.274 53.7289 257.775 50.9551 269 54.071" strokeLinecap="round"/>
                         </svg>
                     </motion.div>
@@ -94,9 +125,9 @@ const MainHeading = ({}) => {
                         </motion.div>
                         <svg className={`width100 row3 col2 span2 ${c.svg}  `} viewBox="0 0 276 55" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <motion.path 
-                            initial={{ pathLength: 0 }}
-                            animate={{ pathLength: 1 }} 
-                            transition={{duration: 1, delay: 0.7}}
+                            initial="hidden"
+                            animate={linkAni}
+                            variants={line}
                             d="M31.8635 25.1662C50.1558 22.0456 68.4147 19.4252 86.332 15.3289C118.321 8.01565 149.672 2.1154 183.724 1.55677C201.824 1.25983 224.199 1.27107 240.707 7.26624C245.304 8.93577 254.758 14.4059 254.881 18.4537C255.181 28.2757 229.105 32.0513 219.903 34.0776C171.276 44.7846 121.314 51.9737 70.1 53.2507C57.4188 53.5668 47.6046 53.6213 35.2357 52.7106C30.506 52.3623 21.2292 51.9122 17.4606 48.93C12.9979 45.3985 16.4495 38.2108 20.0897 35.1964C27.6956 28.8979 41.0931 24.3746 52.2106 21.81C82.7075 14.7748 116.835 10.5726 148.745 8.1921C181.521 5.74698 214.923 6.33114 246.994 11.8184C248.795 12.1266 275.105 16.1553 275 19.2253C274.967 20.167 271.813 21.6118 271.456 21.7714C262.726 25.6743 252.577 28.6438 242.936 31.2615C200.503 42.7824 153.762 48.7362 108.108 49.6629C82.2338 50.1882 54.0633 49.2713 29.0058 44.3007C19.2123 42.3579 10.0761 38.6452 1 35.5821" strokeLinecap="round"/>
                         </svg>
                     </motion.div>
@@ -130,8 +161,8 @@ const MainHeading = ({}) => {
                 {/* <h4 className={`row5 col2 span2 z5 text ${c.paddingtop}`}>industrial design + web dev + UI/UX + graphic design</h4> */}
                 <motion.div 
                 className={`row6 col2__r end1__r z5 ${c.caption} `}
-                initial="hidden2"
-                animate="show2"  
+                initial="hidden"
+                animate={linkAni}  
                 variants={links}
                 >
                     <h4 className="text" >
@@ -142,14 +173,14 @@ const MainHeading = ({}) => {
                     <a href="#work">
                     <svg width="23" height="28" viewBox="0 0 23 28" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" xmlns="http://www.w3.org/2000/svg">
                         <motion.path 
-                        initial={{ pathLength: 0 }}
-                            animate={{ pathLength: 1 }} 
-                            transition={{duration: 0.3, delay: 0.1}}
+                        initial="hidden"
+                        animate={linkAni}
+                        variants={line}
                             d="M11.211 1.34155C10.9258 6.19 11.3931 10.9613 11.4237 15.8067C11.4398 18.3644 11.3535 20.9187 11.6364 23.4648" />
                         <motion.path 
-                        initial={{ pathLength: 0 }}
-                            animate={{ pathLength: 1 }} 
-                            transition={{duration: 0.3, delay: 0.2}}
+                        initial="hidden"
+                        animate={linkAni}
+                        variants={line}
                             d="M1 15.594C2.44192 17.9503 4.25581 20.0564 5.82173 22.3302C6.62219 23.4926 7.64517 24.4532 8.63441 25.4502C9.00712 25.8258 9.80773 27.0473 10.5016 26.892C11.3709 26.6974 12.2628 25.7503 12.9361 25.2375C14.3168 24.186 15.722 23.1606 17.0724 22.0702C18.9255 20.574 19.8713 18.5755 21.4215 16.8703" />
                     </svg>
                     </a>
@@ -161,8 +192,8 @@ const MainHeading = ({}) => {
 
                     </motion.div>
                 <motion.div className={`row7 col2 ${c.links}`}
-                initial="hidden2"
-                animate="show2"  
+                initial="hidden"
+                animate={linkAni}  
                 variants={links}
                 >
                     <motion.p variants={link} ><a href="https://www.instagram.com/art.last.night/">instagram</a> </motion.p>
@@ -170,8 +201,8 @@ const MainHeading = ({}) => {
                     <motion.p variants={link} ><a href="https://www.linkedin.com/in/not-simon/">linkedin</a> </motion.p>
                 </motion.div>
                 <motion.div className={`row7 col3__d  ${c.links}`}
-                initial="hidden2"
-                animate="show2"  
+                initial="hidden"
+                animate={linkAni}  
                 variants={links}
                 >
                     <motion.p variants={link} >©</motion.p>
@@ -182,7 +213,7 @@ const MainHeading = ({}) => {
             </div>
            
             </motion.section>
-            <section  className={c.mainheading__dummy}>
+            <section ref={ref} className={c.mainheading__dummy}>
           
             </section>
         </React.Fragment>
