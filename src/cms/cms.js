b3811c916860967443f6e77a22f7a0ff8daff032
last night <fujizhang2525@gmail.com>
2022-02-17 20:24:10 -0500

    pleasee!!!!!!!!!!!! WORK!!!!1

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+//import uploadcare from 'netlify-cms-media-library-uploadcare'
+//import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
