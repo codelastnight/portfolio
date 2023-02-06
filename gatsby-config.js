018999768b4d7b872e33db86010d19aaed006e5d
last night <fujizhang2525@gmail.com>
2023-02-06 10:32:08 -0500

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-loadable-components-ssr',
+    //  'gatsby-plugin-loadable-components-ssr',
 
     'gatsby-plugin-svgr',
     {
